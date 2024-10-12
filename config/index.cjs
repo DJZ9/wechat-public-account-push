e0380376cf69c060cc3d18d69eb1804d0cf29194
DJZ9 <66509515+DJZ9@users.noreply.github.com>
2024-10-12 16:36:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+  USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx97e634742d18ba87',
 
@@ -21,9 +21,11 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小羊排',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oMOXU6u9s1IE5nYwePzOzxwMI7l4',
+      // id: 'oMOXU6u9s1IE5nYwePzOzxwMI7l4',
+      id: 'PDU32417TLHKumrmNTLlME2GRysrBQnD3cuEyqLst',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '_xpbynzxoKffxXEbZNApI2XT4U0VM4aKf06pv7Pp9h0',
+      // useTemplateId: '_xpbynzxoKffxXEbZNApI2XT4U0VM4aKf06pv7Pp9h0',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-28',
       festivals: [
